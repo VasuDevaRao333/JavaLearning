02024a4e2c83639d53e4e188af194d844cd0aba5
VasuDevaRao333 <vasu333jc@gmail.com>
2023-01-02 15:24:03 +0530

    my first project

---
+++ Learn.java
@@ -0,0 +1,8 @@
+public class Learn {
+    public static void main(String args[]) {
+        System.out.println("Hi Vasudev");
+        String name = "Jackie Chan";
+        // this is string
+        System.out.println(name);
+    }
+}
