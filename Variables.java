277a378d6af473447e26257dbd4cad152a369653
VasuDevaRao333 <vasu333jc@gmail.com>
2023-01-02 16:29:48 +0530

    java variables

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String args[]) {
+        // this is String
+        String name = "Hello Jackie";
+        // this is int
+        int num = 33;
+        // this is float
+        float num1 = 33.33f;
+        // this is char
+        char num2 = 'V';
+        // this is boolean
+        boolean num3 = true;
+
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
