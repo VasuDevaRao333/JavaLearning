4c3e7463b89a82960d117af595f31919f551fede
VasuDevaRao333 <vasu333jc@gmail.com>
2023-01-03 07:45:09 +0530

    My String

---
+++ Mystring.java
@@ -0,0 +1,20 @@
+public class Mystring {
+    public static void main(String args[]) {
+        // "how are you" stored by name variable
+        String name = "how are you";
+        // printed text
+        System.out.println(name);
+
+        // String name.length() (method name)
+        System.out.println(name.length());
+
+        // String name.method name
+        System.out.println(name.toUpperCase());
+
+        // String name.toLowerCase()
+        System.out.println(name.toLowerCase());
+
+        // This method returns the index(the position)
+        System.out.println(name.indexOf("re"));
+    }
+}
