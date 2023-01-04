b7d26f8c9ba321c2184478ee91fab78d8c3e06d2
VasuDevaRao333 <vasu333jc@gmail.com>
2023-01-04 21:45:49 +0530

    Number data type

---
+++ JavaNumber.java
@@ -0,0 +1,21 @@
+public class javaNumber {
+    public static void main(String args[]) {
+        byte myNum = 100;
+        short myNum2 = 5000;
+        int myNum3 = 100000;
+        long myNum4 = 15000000000L;
+        float myNum5 = 5.75f;
+        double myNum6 = 19.99d;
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+
+        System.out.println(myNum);
+        System.out.println(myNum2);
+        System.out.println(myNum3);
+        System.out.println(myNum4);
+        System.out.println(myNum5);
+        System.out.println(myNum6);
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
