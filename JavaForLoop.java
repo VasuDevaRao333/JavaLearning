d93750a069ef0beb6a67145ae9b863122580c064
VasuDevaRao333 <vasu333jc@gmail.com>
2023-01-09 10:21:39 +0530

    my forloop

---
+++ JavaForLoop.java
@@ -0,0 +1,10 @@
+public class JavaForLoop {
+    public static void myMethod(String name, int are) {
+        System.out.println("How are you " + name + " is " + are);
+    }
+
+    public static void main(String args[]) {
+        myMethod("Jackie", 68);
+        myMethod("Vasudev", 22);
+    }
+}
